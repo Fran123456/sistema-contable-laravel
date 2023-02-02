ed9dda3bd16f4fa66ef00893d889d2be7c1933fe
Frank <navasfran98@gmail.com>
2023-02-02 11:10:25 -0600

    notificaciones en dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,67 +1,125 @@
 <x-app-layout>
 
   <div class="container">
+
     <div class="row">
+      <div class="col-md-8 col-xs-12 xol-sm-12">
+        <div class="row">
+          <div class="col-md-6 col-xs-12 col-sm-6 mt-3 ">
+            <div class="card bg-dark text-white ">
+              <a href="#">
+                <img src="{!! asset('banners/informacion.jpg') !!}" class="card-img" alt="...">
+              </a>
+            </div>
+          </div>
 
-      <div class="col-md-4 col-xs-12 col-sm-6 mt-3 ">
-        <div class="card bg-dark text-white ">
-          <a href="#">
-            <img src="{!! asset('banners/informacion.jpg') !!}" class="card-img" alt="...">
-          </a>
+          <div class="col-md-6 col-xs-12 col-sm-6 mt-3" >
+            <div class="card bg-dark text-white">
+              <a href="#">
+                <img src="{!! asset('banners/nominas.jpg') !!}" class="card-img" alt="...">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-xs-12 col-sm-6 mt-3">
+            <div class="card bg-dark text-white">
+              <a href="#">
+                <img src="{!! asset('banners/desarrollo.jpg') !!}" class="card-img" alt="...">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-xs-12 col-sm-6 mt-3">
+            <div class="card bg-dark text-white">
+              <a href="#">
+                <img src="{!! asset('banners/buzon.jpg ') !!}" class="card-img" alt="...">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-xs-12 col-sm-6 mt-3">
+            <div class="card bg-dark text-white">
+              <a href="#">
+                <img src="{!! asset('banners/beneficios.jpg') !!}" class="card-img" alt="...">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-xs-12 col-sm-6 mt-3">
+            <div class="card bg-dark text-white">
+              <a href="#">
+                <img src="{!! asset('banners/encuesta-covid.jpg') !!}" class="card-img" alt="...">
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-xs-12 col-sm-6 mt-3">
+            <div class="card bg-dark text-white">
+              <a href="#">
+                <img src="{!! asset('banners/iso.png') !!}" class="card-img" alt="...">
+              </a>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div class="col-md-4 col-xs-12 col-sm-6 mt-3" >
-        <div class="card bg-dark text-white">
-          <a href="#">
-            <img src="{!! asset('banners/nominas.jpg') !!}" class="card-img" alt="...">
-          </a>
+      <div class="col-md-4 col-xs-12 xol-sm-12 ">
+        <div class="mt-3">
+
+          <div class="card">
+            <h5 class="card-header">NOTIFICACIONES</h5>
+            <div class="card-body">
+
+              <ul class="list-group list-group-light mb-4">
+
+                 <li class="list-group-item d-flex justify-content-between align-items-center">
+                   <a href="#">
+                     <div class="d-flex align-items-center">
+                       <img src="https://mdbootstrap.com/img/new/avatars/8.jpg" alt="" style="width: 45px; height: 45px"
+                         class="rounded-circle" />
+                       <div class="ms-3">
+                         <p class="fw-bold mb-1">John Doe</p>
+                         <p class="text-muted mb-0">Se te asigno una capacitación: NUEVO INSTRUCTIVO DE LA UIF PARA
+                           LA PREVENCIÓN DE LAVADO DE DINERO Y DE ACTIVOS, FINANCIACIÓN DEL TERRORISMO Y
+                            FINANCIACIÓN DE LA PROLIFERACIÓN DE ARMAS DE DESTRUCCIÓN MASIVA
+                           </p>
+                           <small>2022-11-22 09:04:25</small>
+                       </div>
+                     </div>
+                   </a>
+                 </li>
+
+
+
+
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                  <a href="#">
+                    <div class="d-flex align-items-center">
+                      <img src="https://mdbootstrap.com/img/new/avatars/8.jpg" alt="" style="width: 45px; height: 45px"
+                        class="rounded-circle" />
+                      <div class="ms-3">
+                        <p class="fw-bold mb-1">John Doe</p>
+                        <p class="text-muted mb-0">Se te asigno una capacitación: NUEVO INSTRUCTIVO DE LA UIF PARA
+                          LA PREVENCIÓN DE LAVADO DE DINERO Y DE ACTIVOS, FINANCIACIÓN DEL TERRORISMO Y
+                           FINANCIACIÓN DE LA PROLIFERACIÓN DE ARMAS DE DESTRUCCIÓN MASIVA
+                          </p>
+                          <small>2022-11-22 09:04:25</small>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+
+              </ul>
+
+            </div>
+
+            <a href="#"><div class="card-footer text-muted text-center">VER MAS</div></a>
+          </div>
+
         </div>
       </div>
-
-      <div class="col-md-4 col-xs-12 col-sm-6 mt-3">
-        <div class="card bg-dark text-white">
-          <a href="#">
-            <img src="{!! asset('banners/desarrollo.jpg') !!}" class="card-img" alt="...">
-          </a>
-        </div>
-      </div>
-
-      <div class="col-md-4 col-xs-12 col-sm-6 mt-3">
-        <div class="card bg-dark text-white">
-          <a href="#">
-            <img src="{!! asset('banners/buzon.jpg ') !!}" class="card-img" alt="...">
-          </a>
-        </div>
-      </div>
-
-
-      <div class="col-md-4 col-xs-12 col-sm-6 mt-3">
-        <div class="card bg-dark text-white">
-          <a href="#">
-            <img src="{!! asset('banners/beneficios.jpg') !!}" class="card-img" alt="...">
-          </a>
-        </div>
-      </div>
-
-      <div class="col-md-4 col-xs-12 col-sm-6 mt-3">
-        <div class="card bg-dark text-white">
-          <a href="#">
-            <img src="{!! asset('banners/encuesta-covid.jpg') !!}" class="card-img" alt="...">
-          </a>
-        </div>
-      </div>
-
-      <div class="col-md-4 col-xs-12 col-sm-6 mt-3">
-        <div class="card bg-dark text-white">
-          <a href="#">
-            <img src="{!! asset('banners/iso.png') !!}" class="card-img" alt="...">
-          </a>
-        </div>
-      </div>
-
-
     </div>
+
   </div>
 
 </x-app-layout>
