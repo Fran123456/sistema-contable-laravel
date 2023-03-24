3affe5eb7f4623a8805d1a9ff50a13b467c20307
Frank <navasfran98@gmail.com>
2023-03-23 23:04:53 -0600

    fix al registrar usuario y un equipo;

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Jetstream\Jetstream;
 
+
 class CreateNewUser implements CreatesNewUsers
 {
     use PasswordValidationRules;
@@ -34,7 +35,8 @@ class CreateNewUser implements CreatesNewUsers
                 'email' => $input['email'],
                 'password' => Hash::make($input['password']),
             ]), function (User $user) {
-                $this->createTeam($user);
+                 $this->createTeam($user);
+
             });
         });
     }
@@ -44,10 +46,28 @@ class CreateNewUser implements CreatesNewUsers
      */
     protected function createTeam(User $user): void
     {
-        $user->ownedTeams()->save(Team::forceCreate([
+        /*$user->ownedTeams()->save(Team::forceCreate([
             'user_id' => $user->id,
             'name' => explode(' ', $user->name, 2)[0]."'s Team",
             'personal_team' => true,
-        ]));
+        ]));*/
+         $team = DB::table('teams')->insertGetId([
+            'user_id'=> $user->id,
+            'name' => explode(' ', $user->name, 2)[0]."'s Team",
+            'personal_team' => true,
+            'created_at'=>$user->created_at,
+            'updated_at'=> $user->updated_at
+         ]);
+
+        DB::table('team_user')->insert([
+           'user_id'=> $user->id,
+           'team_id'=> $team,
+           'role'=> 'admin',
+           'created_at'=>$user->created_at,
+           'updated_at'=> $user->updated_at
+        ]);
+
+
+
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('current_team_id')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
