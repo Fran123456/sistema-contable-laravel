3e8a468145087f5e9dcb6c2551b2d73999de13f0
Francisco <navasfran98@gmail.com>
2023-05-16 16:47:06 -0600

    listado de empresas por usuario;

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,35 @@
         <x-commonnav  ></x-commonnav>
     </div>
 
-    <h4>Welcome</h4>
+    <h4 class="mb-2">Seleccione empresas para administrar</h4>
+
+    <div class="col-md-12">
+        <div class="row">
+
+        </div>
+        @foreach (Help::usuario()->empresas as $e)
+            
+            <div  class="col-md-4">
+                <div class="card mb-3" @if ($e->id == Help::usuario()->usuario?->empresa_id) style="color:red" @endif >
+                    <div class="row g-0">
+                      <div class="col-md-4">
+                        <img src="..." class="img-fluid rounded-start" alt="...">
+                      </div>
+                      <div class="col-md-8">
+                        <div class="card-body">
+                          <h5 class="card-title">{{ $e->empresa }}</h5>
+                          <p class="card-text">
+                            
+                          </p>
+                         <a href="" class="btn btn-success" style="color:aliceblue">Activar</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+            </div>
+            
+        @endforeach
+    </div>
+    
 
 </x-app-layout>
