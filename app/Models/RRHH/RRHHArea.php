6631f65347d8600273f7d71919d77846ea6b0a37
Katy Pacheco <pachecokaty121@gmail.com>
2024-01-18 20:30:57 -0600

    Crear model, controlller de area, falta la vista

---
+++ app/Models/RRHH/RRHHArea.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\RRHH;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RRHHArea extends Model
+{
+    use HasFactory;
+    protected $table = "rrhh_area";
+    protected $fillable = [
+        'area',
+        'empresa_id',
+        'activo',
+        'created_at',
+        'updated_at',
+    ];
+
+}
