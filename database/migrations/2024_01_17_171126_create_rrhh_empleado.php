3cfc5c3c4f8c72ad867b3682e196c496ef51d555
Cristian Maltez <cristianjosuemaltezrivas@gmail.com>
2024-01-28 17:43:08 -0600

    tarea 29/01/24 migracion empleado

--- database/migrations/2024_01_17_171126_create_rrhh_empleado.php
+++ database/migrations/2024_01_17_171126_create_rrhh_empleado.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('rrhh_empleado', function (Blueprint $table) {
             $table->id();
+            $table->foreign('tipo_empleado_id')->references('id')->on('rrhh_tipo_empleado')->onUpdate('cascade');
             $table->integer('tipo_empleado_id');
             $table->string('nombres', 300);
             $table->string('apellidos', 200);
