c912fb668ecd7a7e19b10c907acba5574e003431
Cristian Maltez <cristianjosuemaltezrivas@gmail.com>
2024-01-29 16:45:57 -0600

    migracion de empleados

--- database/migrations/2024_01_17_171126_create_rrhh_empleado.php
+++ database/migrations/2024_01_17_171126_create_rrhh_empleado.php
@@ -22,13 +22,13 @@ return new class extends Migration
             $table->string('nombre_completo', 300);
             $table->integer('edad');
             $table->boolean('activo')->default(true);
-            $table->string('correo', 200)->nullable();
+            $table->string('correo', 200)->nullable()->nullable();
             $table->string('telefono', 100);
-            $table->string('correo_empresarial', 200);
+            $table->string('correo_empresarial', 200)->nullable();
             $table->text('direccion')->nullable();
             $table->string('sexo', 20)->enum('Femenino', 'Masculino');
-            $table->dropColumn('codigo');
-            $table->dropColumn('foto');
+            $table->string('codigo', 255);
+            $table->text('foto')->nullable();
             $table->date('fecha_nacimiento');
             $table->date('fecha_ingreso')->nullable();
             $table->timestamps();
