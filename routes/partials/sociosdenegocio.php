78722bbbd96995c8a1345a29044a0f4097832ab8
LuigiM <jlpadilla@innovacion.gob.sv>
2024-07-19 20:45:50 -0600

    Feat: seleccionar ids de los contactos y mostrarlos en una vista

--- routes/partials/sociosdenegocio.php
+++ routes/partials/sociosdenegocio.php
@@ -11,50 +11,47 @@ use App\Http\Controllers\SociosdeNegocio\ClienteController;
 Route::middleware(['auth'])->group(function () {
     Route::name('socios.')->prefix('socios')->group(function () {
         Route::resource('contacto', ContactoController::class);
-
-        
+        Route::post('/contacto/selected-ids', [ContactoController::class, 'showSelectedIds'])->name('contacto.selectedIds');
     });
 
     //Rutas de cargos
-    Route::name('socios.')->prefix('socios')->group(function (){
+    Route::name('socios.')->prefix('socios')->group(function () {
         Route::resource('cargo', CargoController::class);
     });
 
     //Ruta de registros
-    Route::name('socios.')->prefix('socios')->group(function (){
+    Route::name('socios.')->prefix('socios')->group(function () {
         Route::resource('registro', RegistroController::class);
     });
 
     //Ruta de Proveedores
-    Route::name('socios.')->prefix('socios')->group(function (){
+    Route::name('socios.')->prefix('socios')->group(function () {
         Route::resource('proveedores', ProveedoresController::class);
         Route::get('/deshabilitar-proveedor/{id}', [ProveedoresController::class, 'deshabilitarProveedor'])->name('.deshabilitarProveedor');
-        Route::get('/habilitar-proveedor/{id}', [ProveedoresController::class, 'habilitarProveedor'])->name('.habilitarProveedor');        
-        
+        Route::get('/habilitar-proveedor/{id}', [ProveedoresController::class, 'habilitarProveedor'])->name('.habilitarProveedor');
+
         //Ruta para listar y crear  productos asociados a proveedores
         Route::get('/listar-producto/{id}', [ProveedoresController::class, 'listarProductos'])->name('listarProductos');
     });
 
     //Ruta de clasificacion de clientes
-    Route::name('socios.')->prefix('socios')->group(function (){
+    Route::name('socios.')->prefix('socios')->group(function () {
         Route::resource('clasificacion', ClasificacionClienteController::class);
     });
 
     //Ruta de clientes
-    Route::name('socios.')->prefix('socios')->group(function (){
+    Route::name('socios.')->prefix('socios')->group(function () {
         Route::resource('cliente', ClienteController::class);
     });
 
     //Ruta que ejecuta el codigo javascript para obtener el departamento y distrito correcto
     Route::name('socios.')->prefix('socios')->group(function () {
-        Route::get('/obtener-departamentos/{paisId}',[ClienteController::class, 'obtenerDepartamentos'])->name('.obtenerDepartamentos');
-        Route::get('/obtener-distritos/{departamentoId}',[ClienteController::class, 'obtenerDistritos'])->name('.obtenerDistritos');
+        Route::get('/obtener-departamentos/{paisId}', [ClienteController::class, 'obtenerDepartamentos'])->name('.obtenerDepartamentos');
+        Route::get('/obtener-distritos/{departamentoId}', [ClienteController::class, 'obtenerDistritos'])->name('.obtenerDistritos');
     });
     //Ruta para habilitar y deshabilitar un cliente
-    Route::name('socios')->prefix('socios')->group(function (){
+    Route::name('socios')->prefix('socios')->group(function () {
         Route::get('/deshabilitar-cliente/{id}', [ClienteController::class, 'deshabilitarCliente'])->name('.deshabilitarCliente');
-        Route::get('/habilitar-cliente/{id}', [ClienteController::class, 'habilitarCliente'])->name('.habilitarCliente');        
+        Route::get('/habilitar-cliente/{id}', [ClienteController::class, 'habilitarCliente'])->name('.habilitarCliente');
     });
 });
-
-
