bcd5475075637277e78b0fccfc97257b3cfb5ab0
Francisco Navas <francisco.navas@datasys.la>
2024-07-24 23:43:02 -0600

    fixes en migraciones

--- database/migrations/2024_07_23_154455_create_fact_estado_facturacion_table.php
+++ database/migrations/2024_07_23_154455_create_fact_estado_facturacion_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
     {
         Schema::create('fact_estado_facturacion', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('estado', 50);
+            $table->timestamps();
+            
         });
     }
 
--- database/migrations/2024_07_23_160759_create_fact_documento_table.php
+++ database/migrations/2024_07_23_160759_create_fact_documento_table.php
@@ -16,16 +16,29 @@ return new class extends Migration
         Schema::create('fact_documento', function (Blueprint $table) {
             $table->id();
             $table->string('documento')->nullable();
-            $table->foreignId('facturacion_id')->constrained('fact_facturacion');
+           
+      
+            $table->unsignedBigInteger('facturacion_id');
+            $table->foreign('facturacion_id')->references('id')->on('fact_facturacion')->onUpdate('cascade');
+
+
             $table->string('serial')->nullable();
             $table->string('tipo_documento_id')->constrained('fact_tipo_documento');
+         
             $table->unsignedBigInteger('cliente_id')->nullable();
+            $table->foreign('cliente_id')->references('id')->on('socios_cliente')->onUpdate('cascade');
+
             $table->unsignedBigInteger('proveedor_id')->nullable();
+            $table->foreign('proveedor_id')->references('id')->on('socios_proveedores')->onUpdate('cascade');
+
             $table->decimal('monto', 10, 2)->nullable();
             $table->foreignId('estado_facturacion_id')->constrainded('fact_estado_facturacion');
             $table->boolean('posteado');
             $table->dateTime('fecha_emision')->nullable();
             $table->foreignId('creado_por')->constrained('users');
+
+            $table->unsignedBigInteger('empresa_id')->nullable();
+            $table->foreign('empresa_id')->references('id')->on('rrhh_empresa')->onUpdate('cascade');
             $table->timestamps();
         });
     }
