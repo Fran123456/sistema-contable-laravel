9e3e6b1b981c60cc412f0bdf5c684d5649b1d512
jose artero <joseartero31@gmail.com>
2024-07-30 11:29:07 -0600

    integracion de migraciones y modelos de balance de estado

---
+++ database/migrations/2024_07_29_162357_create_conta_grupo_sub_resultado_rpt_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conta_grupo_sub_resultado_rpt', function (Blueprint $table) {
+            $table->id();
+            $table->string('sub_grupo', 200)->nullable();
+            $table->unsignedBigInteger('grupo_id')->nullable();
+            $table->unsignedBigInteger('utilidad_id')->nullable();
+            $table->foreign('grupo_id')->references('id')->on('conta_grupo_resultado_rpt')->onUpdate('cascade');
+            $table->foreign('utilidad_id')->references('id')->on('conta_utilidad_rpt')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conta_grupo_sub_resultado_rpt');
+    }
+};
